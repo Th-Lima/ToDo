d80fa86e979441975082e25c27f591586acf2fcc
Thales Lima <lthales53@gmail.com>
2021-01-08 19:41:53 -0300

    Implementing mask and confirm messages

--- TodoApp/wwwroot/js/site.js
+++ TodoApp/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    Mask();
+    AlertIsDone();
+    AlertDelete();
+});
+
+function Mask() {
+    $(".dateConclusion").mask("99/99/9999");
+}
+
+function AlertDelete() {
+    $(".deleteTask").click(function (e) {
+        var result = confirm("Deseja realmente concluir esta ação?");
+
+        if (result == false) {
+            e.preventDefault();
+        }
+    });
+}
+
+function AlertIsDone() {
+    $(".btnIsDone").click(function (e) {
+        var result = confirm("Deseja cumprir esta tarefa?");
+
+        if (result == false) {
+            e.preventDefault();
+        }
+    });
+}
