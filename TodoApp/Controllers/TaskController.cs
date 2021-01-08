e0ec1922c9fc6c20063aa44be0126fd43bd6059c
Thales Lima <lthales53@gmail.com>
2021-01-08 19:22:53 -0300

    Creating controller for task

---
+++ TodoApp/Controllers/TaskController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Helpers;
+using TodoApp.Libraries.Messages;
+using TodoApp.Models;
+using TodoApp.Repositories.Contracts;
+
+namespace TodoApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        private ITaskRepository _taskRepository;
+       
+        public TaskController(ITaskRepository taskController)
+        {
+            _taskRepository = taskController;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_taskRepository.GetAllTasks());
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromForm] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DateHelper.VerifyDate(task))
+                {
+                    TempData["MSG_E"] = Message.MSG_E004;
+                    return View();
+                }
+                _taskRepository.Register(task);
+                TempData["MSG_S"] = Message.MSG_S001;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var task = _taskRepository.GetTask(id);
+
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromForm] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DateHelper.VerifyDate(task))
+                {
+                    TempData["MSG_E"] = Message.MSG_E004;
+                    return View();
+                }
+                _taskRepository.Updade(task);
+                TempData["MSG_S"] = Message.MSG_S001;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        public IActionResult IsDone(int id)
+        {
+            Task task = _taskRepository.GetTask(id);
+            if (!task.IsDone)
+            {
+                task.IsDone = true;
+            }
+
+            _taskRepository.Updade(task);
+
+            TempData["MSG_S"] = Message.MSG_S003;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _taskRepository.Delete(id);
+
+            TempData["MSG_S"] = Message.MSG_S002;
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- TodoApp/Models/Task.cs
+++ TodoApp/Models/Task.cs
@@ -14,15 +14,25 @@ namespace TodoApp.Models
 
         [Required(ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
         [Display(Name = "Título")]
+        [MinLength(2, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E002")]
+        [MaxLength(20, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E003")]
         public string Title { get; set; }
 
         [Display(Name = "Subtitulo")]
+        [MinLength(2, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E002")]
+        [MaxLength(20, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E003")]
         public string Subtitle { get; set; }
 
         [Required(ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(10, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E002")]
+        [MaxLength(200, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E003")]
         [Display(Name = "Descrição")]
         public string Description { get; set; }
-        
+
+        [Display(Name = "Data de conclusão")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime ConclusionDate { get; set; }
+
         public bool IsDone { get; set; }
     }
 }
