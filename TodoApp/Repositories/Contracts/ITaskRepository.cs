00d9a9438f4554d838e9db970b2be9e8a009fb3a
Thales Lima <lthales53@gmail.com>
2021-01-08 19:34:54 -0300

    Implementing repository pattern

---
+++ TodoApp/Repositories/Contracts/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApp.Models;
+
+namespace TodoApp.Repositories.Contracts
+{
+    public interface ITaskRepository
+    {
+        //CRUD
+        void Register(Task task);
+        void Updade(Task task);
+        Task GetTask(int id);
+        void Delete(int id);
+        IEnumerable<Task> GetAllTasks();
+    }
+}
---
+++ TodoApp/Repositories/TaskRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using TodoApp.Data;
+using TodoApp.Models;
+using TodoApp.Repositories.Contracts;
+
+namespace TodoApp.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private TodoContext _db;
+        public TaskRepository(TodoContext db)
+        {
+            _db = db;
+        }
+
+        public void Register(Task task)
+        {
+            _db.Add(task);
+            _db.SaveChanges();
+        }
+
+        public void Updade(Task task)
+        {
+            _db.Update(task);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Task task = GetTask(id);
+            _db.Remove(task);
+            _db.SaveChanges();
+        }
+
+        public Task GetTask(int id)
+        {
+            return _db.Tasks.Find(id);
+        }
+
+        public IEnumerable<Task> GetAllTasks()
+        {
+            return _db.Tasks;
+        }
+    }
+}
