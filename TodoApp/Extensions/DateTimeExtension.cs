0ae3d63e5ebfc0c63fa4e760373386f9ad918201
Thales Lima <lthales53@gmail.com>
2021-10-11 20:30:44 -0300

    Modify Helper for the Extension class and method

---
+++ TodoApp/Extensions/DateTimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TodoApp.Extensions
+{
+    public static class DateTimeExtension 
+    {
+        public static bool VerifyDate(this DateTime date)
+        {
+            if (date.Date < DateTime.Now.Date)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
